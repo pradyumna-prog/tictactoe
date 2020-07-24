6bf07606d4eb76324494a86bcb76a1b32c6c974c
pradyumna-prog <pradyumnatripathi99@gmail.com>
2020-07-24 19:23:53 +0530

    code refactoring

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ let turn;
 let dark;
 let result, player;
 
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-}
-
 function switchTheme(){
     dark = !dark
     if(dark){
